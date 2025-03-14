0d5febcd48875d812d18725a00c94a7376da9e6d
Lemon <bxb100@outlook.com>
2025-03-14 17:06:37 +0800

    Initial commit

---
+++ src/main/java/com/example/sasexample/SasExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sasexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasExampleApplication.class, args);
+    }
+
+}
